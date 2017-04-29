9b97f33140f96fe24f3e4d73b401e8c0666175a1
FrogBomb <TLBlanchet@gmail.com>
2017-04-29 11:57:01 -0600

    VOICE RESPONSE!
    THIS WAS WORTH THE TIME!

--- back_bot.py
+++ back_bot.py
@@ -1,5 +1,7 @@
-#import discord
+import discord
 from discord.ext.commands import Bot
+import discord.opus as opus
+import time
 
 with open("super_secret_key.txt") as f:
     key = f.readlines()[0].rstrip()
@@ -13,10 +15,31 @@ async def on_read():
     
 @back_bot.event
 async def on_message(message):
-    #it's a good bug
         if(("back" in message.content.lower()) and (message.author.id != back_bot.user.id)):
             print("back found!")
-            await back_bot.send_message(message.channel, "Did somebody say back?")
+            if(opus.is_loaded() and isinstance(message.author, discord.Member) and message.author.voice.voice_channel != None):
+                try:
+                    voice_client = await back_bot.join_voice_channel(message.author.voice.voice_channel)
+                except:
+                    voice_client = back_bot.voice_client_in(message.author.server)
+                    await voice_client.move_to(message.author.voice.voice_channel)
+                
+                try:
+                    def disconnect_from_vc(*args):
+                        voice_client.disconnet()
+                        
+                    player = voice_client.create_ffmpeg_player('did_sombody_say_back.opus', after = disconnect_from_vc)
+                    
+                    player.start()
+                    time.sleep(2)
+                    await voice_client.disconnect()
+                    
+                except Exception as e:
+                    await voice_client.disconnect()
+                    await back_bot.send_message(message.channel, "Did somebody say back?")
+                    raise e
+            else:
+                await back_bot.send_message(message.channel, "Did somebody say back?")
         await back_bot.process_commands(message)
 #
 @back_bot.command()
